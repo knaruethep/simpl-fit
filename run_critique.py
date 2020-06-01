065346a989d171b10b09871a1c9bdea1e1ece494
sanil-shrvn <60662882+sanil-shrvn@users.noreply.github.com>
2020-05-31 20:23:23 -0700

    Initial Commit

---
+++ run_critique.py
@@ -0,0 +1,99 @@
+import argparse
+import logging
+import time
+import base64
+import cv2
+import numpy as np
+import imutils
+import sys
+import os
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+from vision import analyze
+
+logger = logging.getLogger('TfPoseEstimator-WebCam')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+fps_time = 0
+
+# Load model
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation realtime webcam')
+    parser.add_argument('--camera', type=int, default=0)
+
+    parser.add_argument('--resize', type=str, default='0x0',
+                        help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    
+    parser.add_argument('--tensorrt', type=str, default="False",
+                        help='for tensorrt process.')
+    parser.add_argument('--workout', type=str, default="shoulderpress",
+                        help='shoulderpress, plank, curls, squats, pushup')
+    parser.add_argument('--side', type=str, default="L", help='L for left or R for right')
+    args = parser.parse_args()
+
+    logger.debug('Initializating model %s : %s' % (args.model, get_graph_path(args.model)))
+    w, h = model_wh(args.resize)
+    if w > 0 and h > 0:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h), trt_bool=str2bool(args.tensorrt))
+    else:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368), trt_bool=str2bool(args.tensorrt))
+    logger.debug('Camera read')
+    cam = cv2.VideoCapture(args.camera)
+    ret_val, image = cam.read()
+    logger.info('Camera image=%dx%d' % (image.shape[1], image.shape[0]))
+
+    prev_state = 0 #rest position
+    rep_count = 0
+    while True:
+        ret_val, image = cam.read()
+        logger.debug('Pose Estimation')
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+        body_parts = analyze.extract_body_parts(humans, image)
+        if body_parts == -1:
+            deviation = -1.0000
+            critique = "No critique"
+            state = prev_state
+        else:
+            deviation, critique, state = analyze.analyze_workout(body_parts, args.workout, prev_state, args.side)
+        if prev_state == 2 and state == 1:
+            rep_count += 1
+        logger.debug('Crtique Assignment')
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        if state == 0:
+            State = "rest"
+        elif state == 1:
+            State = "up"
+        elif state == 2:
+            State = "down"
+
+        logger.debug('Displaying')
+        cv2.putText(image,"FPS: %f" % (1.0 / (time.time() - fps_time)),(10, 15),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 2)
+        cv2.putText(image,"Workout: %s" %args.workout ,(10, 45),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 0, 0), 2)
+        #cv2.putText(image,"Deviation: %f" %deviation ,(10, 65),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 2)
+        cv2.putText(image,"Critique: %s" %critique ,(10, 85),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.putText(image,"State: %s" %State ,(10, 105),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.putText(image,"Rep Count: %s" %rep_count ,(10, 125),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.imshow('SimpL', image)
+        fps_time = time.time()
+        if cv2.waitKey(1) == 27:
+            break
+        logger.debug('finished+')
+
+    cv2.destroyAllWindows()
+logger.debug('Terminated Successfully')
---
+++ run_video_cr.py
@@ -0,0 +1,115 @@
+import argparse
+import logging
+import time
+import base64
+import cv2
+import numpy as np
+import imutils
+import sys
+import os
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+from vision import analyze
+
+logger = logging.getLogger('TfPoseEstimator-Video')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
+    parser.add_argument('--video', type=str, default='')
+    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    parser.add_argument('--workout', type=str, default="shoulderpress",
+                        help='shoulderpress, plank, curls, squats, pushup')
+    parser.add_argument('--side', type=str, default="L", help='L for left or R for right')
+    parser.add_argument('--output', type=str, help='A file or directory to save output visualizations.')
+    args = parser.parse_args()
+
+    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
+    prev_state = 0 #rest position
+    rep_count = 0
+    w, h = model_wh(args.resolution)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    cap = cv2.VideoCapture(args.video)
+    frames_per_second = cap.get(cv2.CAP_PROP_FPS)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    if cap.isOpened() is False:
+        print("Error opening video stream or file")
+    while cap.isOpened():
+        ret_val, image = cap.read()
+        logger.debug('Pose Estimation')
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=4.0)
+        body_parts = analyze.extract_body_parts(humans, image)
+        if body_parts == -1:
+            deviation = -1.0000
+            critique = "No critique"
+            state = prev_state
+        else:
+            deviation, critique, state = analyze.analyze_workout(body_parts, args.workout, prev_state, args.side)
+        if prev_state == 2 and state == 1:
+            rep_count += 1
+        logger.debug('Crtique Assignment')
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        if state == 0:
+            State = "rest"
+        elif state == 1:
+            State = "up"
+        elif state == 2:
+            State = "down"
+        cv2.putText(image,"FPS: %f" % (1.0 / (time.time() - fps_time)),(10, 15),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 2)
+        cv2.putText(image,"Workout: %s" %args.workout ,(10, 45),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 0, 0), 2)
+        #cv2.putText(image,"Deviation: %f" %deviation ,(10, 65),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 2)
+        cv2.putText(image,"Critique: %s" %critique ,(10, 85),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.putText(image,"State: %s" %State ,(10, 105),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.putText(image,"Rep Count: %s" %rep_count ,(10, 125),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
+        cv2.imshow('SimpL', image)
+        fps_time = time.time()
+        if cv2.waitKey(1) == 27:
+            break
+        
+        if args.output:
+            if os.path.isdir(args.output):
+                output_fname = os.path.join(args.output, basename)
+                output_fname = os.path.splitext(output_fname)[0] + ".mkv"
+            else:
+                output_fname = args.output
+            assert not os.path.isfile(output_fname), output_fname
+            output_file = cv2.VideoWriter(
+                filename=output_fname,
+                # some installation of opencv may not support x264 (due to its license),
+                # you can try other format (e.g. MPEG)
+                fourcc=cv2.VideoWriter_fourcc(*"x264"),
+                fps=float(frames_per_second),
+                frameSize=(width, height),
+                isColor=True,
+            )
+        assert os.path.isfile(args.video_input)
+        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
+            if args.output:
+                output_file.write(vis_frame)
+            else:
+                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
+                cv2.imshow(basename, vis_frame)
+                if cv2.waitKey(1) == 27:
+                    break  # esc to quit
+        video.release()
+        if args.output:
+            output_file.release()
+        else:
+            cv2.destroyAllWindows()
+
+
+    cv2.destroyAllWindows()
+logger.debug('Terminated Successfully')
